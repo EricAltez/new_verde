91fdde5be71ee2faf92fb3b5746038c7b8cfe2e8
EricAltez <ericaltez1111@gmail.com>
2022-06-20 02:39:52 -0300

    add loading animation and some error cases

--- lib/widgets/loginwidget.dart
+++ lib/widgets/loginwidget.dart
@@ -1,5 +1,6 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:new_verde/main.dart';
 
 class LoginWidget extends StatefulWidget {
   @override
@@ -19,45 +20,59 @@ class _LoginWidgetState extends State<LoginWidget> {
   }
 
   @override
-  Widget build(BuildContext context) => SingleChildScrollView (
-    padding: const EdgeInsets.all(16),
-    child: Column(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: [
-        const SizedBox(height: 40),
-        TextField(
-          controller: emailController,
-          cursorColor: Colors.black,
-          textInputAction: TextInputAction.next,
-          decoration: const InputDecoration(labelText: 'Email'),
+  Widget build(BuildContext context) => SingleChildScrollView(
+        padding: const EdgeInsets.all(16),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const SizedBox(height: 40),
+            TextField(
+              controller: emailController,
+              cursorColor: Colors.black,
+              textInputAction: TextInputAction.next,
+              decoration: const InputDecoration(labelText: 'Email'),
+            ),
+            const SizedBox(height: 4),
+            TextField(
+              controller: passwordController,
+              textInputAction: TextInputAction.done,
+              decoration: const InputDecoration(labelText: 'Password'),
+              obscureText: true,
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            ElevatedButton.icon(
+              style: ElevatedButton.styleFrom(
+                minimumSize: const Size.fromHeight(50),
+              ),
+              icon: const Icon(Icons.lock_open, size: 32),
+              label: const Text(
+                'Sign In',
+                style: TextStyle(fontSize: 24),
+              ),
+              onPressed: signIn,
+            ),
+          ],
         ),
-        const SizedBox(height: 4),
-        TextField(
-          controller: passwordController,
-          textInputAction: TextInputAction.done,
-          decoration: const InputDecoration(labelText: 'Password'),
-          obscureText: true,
-        ),
-        const SizedBox(height: 20,),
-        ElevatedButton.icon(
-          style: ElevatedButton.styleFrom(
-            minimumSize: const Size.fromHeight(50),
-          ),
-          icon: const Icon(Icons.lock_open, size: 32),
-          label: const Text (
-            'Sign In',
-            style: TextStyle(fontSize: 24),
-          ),
-          onPressed: signIn,
-        ),
-      ],
-    ),
-  );
+      );
 
   Future signIn() async {
-    await FirebaseAuth.instance.signInWithEmailAndPassword(
-      email: emailController.text.trim(),
-      password: passwordController.text.trim(),
+    showDialog(
+      context: context,
+      barrierDismissible: false,
+      builder: (context) => const Center(child: CircularProgressIndicator()),
     );
+
+    try {
+      await FirebaseAuth.instance.signInWithEmailAndPassword(
+        email: emailController.text.trim(),
+        password: passwordController.text.trim(),
+      );
+    } on FirebaseAuthException catch (e) {
+      print(e);
+    }
+
+    navigatorKey.currentState!.popUntil((route) => route.isFirst);
   }
 }
