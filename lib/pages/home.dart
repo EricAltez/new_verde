9364a823165b88a7a62dd3248e5c60658223fb65
EricAltez <ericaltez1111@gmail.com>
2022-06-22 03:44:54 -0300

    working on user page

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,5 +1,6 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:new_verde/pages/user_form.dart';
 
 class UserPage extends StatelessWidget {
   const UserPage({Key? key}) : super(key: key);
@@ -7,37 +8,53 @@ class UserPage extends StatelessWidget {
   Widget build(BuildContext context) {
     final user = FirebaseAuth.instance.currentUser!;
     return Scaffold(
-        appBar: AppBar(
-          title: const Text('User Page'),
-        ),
-        body: Padding(
-          padding: const EdgeInsets.all(32),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              const Text(
-                'Signed in as',
-                style: TextStyle(fontSize: 16),
+      appBar: AppBar(
+        title: const Text('User Page'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(32),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Text(
+              'Signed in as',
+              style: TextStyle(fontSize: 16),
+            ),
+            const SizedBox(height: 16),
+            Text(
+              user.email!,
+              style: const TextStyle(fontSize: 20),
+            ),
+            const SizedBox(height: 40),
+            ElevatedButton.icon(
+              style: ElevatedButton.styleFrom(
+                minimumSize: const Size.fromHeight(50),
               ),
-              const SizedBox(height: 16),
-              Text(
-                user.email!,
-                style: const TextStyle(fontSize: 20),
+              icon: const Icon(Icons.arrow_back, size: 32),
+              label: const Text(
+                'Sign Out',
+                style: TextStyle(fontSize: 24),
               ),
-              const SizedBox(height: 40),
-              ElevatedButton.icon(
+              onPressed: () => FirebaseAuth.instance.signOut(),
+            ),
+            ElevatedButton.icon(
                 style: ElevatedButton.styleFrom(
                   minimumSize: const Size.fromHeight(50),
                 ),
                 icon: const Icon(Icons.arrow_back, size: 32),
                 label: const Text(
-                  'Sign Out',
+                  'User data',
                   style: TextStyle(fontSize: 24),
                 ),
-                onPressed: () => FirebaseAuth.instance.signOut(),
-              ),
-            ],
-          ),
-        ));
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => const UserForm()),
+                  );
+                }),
+          ],
+        ),
+      ),
+    );
   }
 }
