6b80a176c42acfc4a1b152559b1a9459b01c9961
Marcosozhol <3956@holbertonschool.com>
2022-06-28 00:51:36 -0300

    botones qr

--- lib/pages/scanpage.dart
+++ lib/pages/scanpage.dart
@@ -1,33 +1,72 @@
+import 'dart:html';
+
 import 'package:flutter/material.dart';
 
 class ScanPage extends StatelessWidget {
   const ScanPage({Key? key}) : super(key: key);
 
+  /*@override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+          child: InkWell(
+            borderRadius: BorderRadius.circular(28),
+            splashColor: Colors.black26,
+            onTap: () {},
+            child: Ink.image(
+              image: AssetImage('images/manzana.jpg'),
+                  height: 200,
+                  width: 200,
+                  fit: BoxFit.cover,
+                  child: Text(
+                    'Orgánicos',
+                    style: TextStyle(fontSize: 32, color: Colors.white),
+                  ),
+                ),
+          )
+          ),
+    );
+  }
+}*/
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Row(
+      body: Column(
         children: [
           const Center(
-            child: Text('scan'),
+            child: Text('Que vas a reciclar?'),
           ),
-          TextButton(
-            child: const Text('Pop!'),
-            onPressed: () {
-              Navigator.pop(context);
-            },
-          ),
-          ElevatedButton(
+          SizedBox(height: 20,),
+          MaterialButton(
+            minWidth: 200,
+            height: 200,
+            color: Colors.lightGreen,
             onPressed: () => Navigator.pushNamed(context, 'scanorganico'),
-            child: null,
+            child: Text('Escanear Organicos',style: TextStyle(color: Colors.white))
           ),
-          ElevatedButton(
+          SizedBox(height: 20,),
+          MaterialButton(
+            minWidth: 200,
+            height: 200,
+            color: Colors.lightGreen,
             onPressed: () => Navigator.pushNamed(context, 'scanplastico'),
-            child: null,
+            child: Text('Escanear plásticos',style: TextStyle(color: Colors.white))
           ),
-          ElevatedButton(
+          SizedBox(height: 20,),
+          MaterialButton(
+            minWidth: 200,
+            height: 200,
+            color: Colors.lightGreen,
             onPressed: () => Navigator.pushNamed(context, 'scancarton'),
-            child: null,
+            child: Text('Escanear cartón',style: TextStyle(color: Colors.white))
+          ),
+          SizedBox(height: 20,),
+          ElevatedButton(
+            child: const Text('Volver al inicio'),
+            onPressed: () {
+            Navigator.pop(context);
+            },
           ),
         ],
       ),
